b1a141e5114e52b38b3f3c78e56a5e7336c1a121
Yoichi Tagaya <yoichi@el-eleven.com>
2015-03-08 01:19:26 +0900

    Add a framework target and build schemes to support installation by Carthage.

---
+++ BWWalkthrough/BWWalkthrough.h
@@ -0,0 +1,19 @@
+//
+//  BWWalkthrough.h
+//  BWWalkthrough
+//
+//  Created by Yari D'areglia on 08/03/15.
+//  Copyright (c) 2015 Yari D'areglia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BWWalkthrough.
+FOUNDATION_EXPORT double BWWalkthroughVersionNumber;
+
+//! Project version string for BWWalkthrough.
+FOUNDATION_EXPORT const unsigned char BWWalkthroughVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BWWalkthrough/PublicHeader.h>
+
+
